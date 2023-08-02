d65da4b6685660540d8b46fd7682cd108a27378b
rzfhlv <rzfhlv.me@gmail.com>
2023-08-02 20:27:46 +0700

    move connection from config to adapter

--- adapter/postgres/postgres.go
+++ adapter/postgres/postgres.go
@@ -1,4 +1,4 @@
-package config
+package postgres
 
 import (
 	"fmt"
@@ -7,16 +7,32 @@ import (
 	"github.com/jmoiron/sqlx"
 )
 
-func NewPostgres() (*sqlx.DB, error) {
-	// connect to postgres
+type Postgres struct {
+	db *sqlx.DB
+}
+
+func NewPostgres() (*Postgres, error) {
 	db, err := sqlx.Open("postgres", fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable", os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME")))
 	if err != nil {
 		return nil, err
 	}
+
 	err = db.Ping()
 	if err != nil {
 		return nil, err
 	}
 
-	return db, nil
+	return &Postgres{
+		db: db,
+	}, nil
+}
+
+func (p *Postgres) GetDB() *sqlx.DB {
+	return p.db
+}
+
+func (p *Postgres) Close() {
+	if p.db != nil {
+		p.db.Close()
+	}
 }
